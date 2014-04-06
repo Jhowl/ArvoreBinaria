4f504342bba3ef8833d3c07e0229f6287f1637d5
Jhonatan <jhonatansouza@outlook.com>
2014-04-06 20:28:22 -0300

    Tree

---
+++ WindowsFormsApplication2/ArvoreBin.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication2
+{
+    class ArvoreBin
+    { 
+        private Nodo raiz = null;// raiz da árvore
+        
+        private int qtde = 0;// qtde de nos internos
+        
+        private string resultado ="";
+        
+        public int qtde_nos_internos() // devolve a qtde de nós internos
+        { 
+            return qtde;
+        }
+        
+        public bool no_eh_externo(Nodo no)// verifica se um determinado Nodo é externo
+        { 
+            return(no.get_no_direita() ==null) && (no.get_no_esquerda() ==null);
+        }
+        
+        public Nodo cria_No_externo(Nodo Nopai) // cria um Nodo externo
+        {
+            Nodo no = new Nodo();
+            no.set_no_pai(Nopai);
+            return no;
+        }
+
+        public void insere(int valor)// insere um valor int
+        {
+            Nodo no_aux;
+            
+            if(qtde == 0)
+            {
+                // árvore vazia, devemos criar o primeiro Nodo, que será a raiz
+                no_aux = new Nodo();
+                raiz = no_aux;
+            }
+            else
+            {
+                // localiza onde deve ser inserido o novo nó.
+                no_aux = raiz;
+                while(no_eh_externo(no_aux) == false)
+                {
+                    if(valor > no_aux.get_valor())
+                        no_aux = no_aux.get_no_direita();
+                    else
+                        no_aux = no_aux.get_no_esquerda();
+                }
+            }
+            // este era um Nodo externo e portanto não tinha filhos.
+            // Agora ele passará a ter valor. Também devemos criar outros 2
+            // Nodos externos (filhos) para ele.
+            no_aux.set_valor(valor);
+       
+            no_aux.set_no_direita(cria_No_externo(no_aux));
+        
+            no_aux.set_no_esquerda(cria_No_externo(no_aux));
+        
+            qtde++;
+        }
+        
+        private void Le_Nodo(Nodo no)
+        {
+            if(no_eh_externo(no))
+                return;
+        
+            Le_Nodo(no.get_no_esquerda());
+            
+            resultado = resultado +" - "+Convert.ToInt32(no.get_valor());
+            Le_Nodo(no.get_no_direita());
+        }
+
+        // devolve um string com os elementos da árvore, em ordem crescentepublic 
+        public string listagem()
+        {
+            resultado ="";
+            Le_Nodo(raiz);
+        
+            return resultado;
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private ArvoreBin minhaArvore = new ArvoreBin();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                minhaArvore.insere(Convert.ToInt32(txtValor.Text));
+                listBox1.Items.Add("Inserido: " + txtValor.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Valor inválido! Digite apenas números!");
+            }
+            txtValor.Clear();
+            txtValor.Focus();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(minhaArvore.listagem());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Qtde: " + minhaArvore.qtde_nos_internos());
+        }
+    }
+}
